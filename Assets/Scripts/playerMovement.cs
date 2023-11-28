708163bce616e798bebf9616c4f59cc3ce53c7c5
G-VosFox <144125620+G-VosFox@users.noreply.github.com>
2023-11-28 14:49:20 +0100

    i can jump
    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH

--- Assets/Scripts/playerMovement.cs
+++ Assets/Scripts/playerMovement.cs
@@ -1,45 +1,61 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 public class playerMovement : MonoBehaviour
 {
-
     private float currentSpeed;
     public float walkSpeed = 10f;
     public float runSpeed = 15f;
+    public float gravity = -0.005f;
+    private float baseGravity;
+    private bool jumped = false;
 
-    public float gravity = -0.5f;
 
     private float moveX;
     private float moveZ;
     private Vector3 move;
 
     public CharacterController characterController;
-
     // Start is called before the first frame update
     void Start()
     {
         currentSpeed = walkSpeed;
+        baseGravity = gravity;
     }
-
     // Update is called once per frame
     void Update()
     {
         moveX = Input.GetAxis("Horizontal") * currentSpeed * Time.deltaTime;
         moveZ = Input.GetAxis("Vertical") * currentSpeed * Time.deltaTime;
-
-        move = transform.right*moveX+ 
-               transform.up*gravity+ 
-               transform.forward*moveZ;
+        move = transform.right * moveX +
+               transform.up * baseGravity +
+               transform.forward * moveZ;
         characterController.Move(move);
 
-        if (Input.GetKey(KeyCode.LeftShift)){
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
             currentSpeed = runSpeed;
         }
         else
         {
             currentSpeed = walkSpeed;
         }
+
+        if (Input.GetKey(KeyCode.Space) && characterController.isGrounded)
+        {
+            Debug.Log("jump");
+            baseGravity = -gravity;
+            jumped = true;
+        }
+        if (!characterController.isGrounded && jumped == true) 
+        {
+            StartCoroutine(GetDown());
+            jumped = false;
+        }
+    }
+    IEnumerator GetDown()
+    {
+        yield return new WaitForSeconds(0.4f);
+        baseGravity = gravity;
     }
 }
---
+++ Assets/Scripts/shooting.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shooting : MonoBehaviour
+{
+    private Ray ray;
+    RaycastHit hit;
+    public Camera cam;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            ray = cam.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.collider.tag.Equals("NPC"))
+                {
+                    Destroy(hit.collider.gameObject);
+                }
+            }
+        }
+    }
+}
