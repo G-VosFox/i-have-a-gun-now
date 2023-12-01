ddc5016099b43790f9c23448c5b706b830266f5f
G-VosFox <144125620+G-VosFox@users.noreply.github.com>
2023-12-01 09:58:34 +0100

    enemy can see player now

--- Assets/Scripts/enemyMovement.cs
+++ Assets/Scripts/enemyMovement.cs
@@ -3,20 +3,44 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Security.Cryptography;
 using UnityEngine;
+using UnityEngine.UIElements;
 using Random = UnityEngine.Random;
 
 public class enemyMovement : MonoBehaviour
 {
-    private Vector3 target;
-    private Vector3 movement;
+    public Vector3 target;
+    public Vector3 current;
+    public GameObject player;
+    private bool spotted = false;
+    public float speed = 10f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        target = new Vector3(Random.Range(-50f, 50f), 0, Random.Range(-50f, 50f));
     }
     // Update is called once per frame
     void Update()
     {
-        target = new Vector3(Random.Range(-50f, 50f), 0, Random.Range(-50f, 50f));
+        current = gameObject.transform.position;
+        var step = speed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, target, step);
+
+        float dist = Vector3.Distance(player.transform.position, transform.position);
+
+        if (dist <= 10)
+        {
+            target = player.transform.position;
+            spotted = true;
+        }
+        else if (dist > 10 && spotted == true)
+        {
+            target = new Vector3(Random.Range(-50f, 50f), 1.5f, Random.Range(-50f, 50f));
+            spotted = false;
+        }
+
+        if (current == target)
+        {
+            target = new Vector3(Random.Range(-50f, 50f), 1.5f, Random.Range(-50f, 50f));
+        }
     }
 }
